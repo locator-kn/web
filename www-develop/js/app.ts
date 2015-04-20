3f437e57bf258f9dd6d69bce93ce8337e7ad932e
DennisMoosmann <dennis.moosmann@gmx.de>
2015-04-20 15:40:58 +0200

    Add person buttons

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -2,6 +2,9 @@
 /// <reference path="../../typings/angular-translate/angular-translate.d.ts" />
 /// <reference path="../../typings/moment/moment.d.ts" />
 
+/// <reference path="./controller/triplerCtrl.ts" />
+/// <reference path="./service/triplerService.ts" />
+
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
--- www-develop/js/service/triplerService.ts
+++ www-develop/js/service/triplerService.ts
@@ -1,7 +1,31 @@
 module Service {
     export class TriplerService {
-        constructor() {
+        constructor(private $q) {
 
+        }
+
+        cities() {
+            return this.$q(function (resolve) {
+                setTimeout(function () {
+                    resolve([
+                        {
+                            "_id": "551bd8d98fe8ee54fe79fbe0",
+                            "name": "Konstanz",
+                            "plz": 78764
+                        },
+                        {
+                            "_id": "551bd8d96ac6271311173b03",
+                            "name": "Berlin",
+                            "plz": 13545
+                        },
+                        {
+                            "_id": "551bd8d9c528474af52dbd7e",
+                            "name": "Hamburg",
+                            "plz": 24567
+                        }
+                    ]);
+                }, 500);
+            });
         }
         static serviceId:string="TriplerService";
     }
