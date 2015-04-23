bffb9bbe231b35513f88248afb247a740adeaac8
Timo Weiß <info@timo-weiss.com>
2015-04-23 18:52:08 +0200

    remove unused gitcheck

--- gulpfile.js
+++ gulpfile.js
@@ -86,23 +86,9 @@ gulp.task('watch', ['ts', 'html', 'css', 'lib', 'locale'], function () {
 });
 
 
-gulp.task('install', ['git-check'], function () {
+gulp.task('install', function () {
     return bower.commands.install()
         .on('log', function (data) {
             gutil.log('bower', gutil.colors.cyan(data.id), data.message);
         });
-});
-
-
-gulp.task('git-check', function (done) {
-    if (!sh.which('git')) {
-        console.log(
-            '  ' + gutil.colors.red('Git is not installed.'),
-            '\n  Git, the version control system, is required to download Ionic.',
-            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-        );
-        process.exit(1);
-    }
-    done();
 });
