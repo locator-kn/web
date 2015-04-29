d713cc195aecde765d23965740324ce1e7223e76
Michael Knoch <info@michaelknoch.de>
2015-04-29 16:08:53 +0200

    use ngdialog

--- www-develop/js/controller/createTripCtrl.ts
+++ www-develop/js/controller/createTripCtrl.ts
@@ -7,11 +7,17 @@ module Controller {
         }
 
 
-        constructor($scope, DataService) {
+        constructor($scope, private DataService, private ngDialog) {
             // initial loading of todays and todays + 3 date
             this.params.checkin = DataService.getDates().checkinDate;
             this.params.checkout = DataService.getDates().checkoutDate;
         }
+
+        open() {
+            this.ngDialog.open({template: './templates/modal/locations.html'});
+            console.info("yo");
+        }
+
 
         static controllerId:string = "CreateTripCtrl";
     }
