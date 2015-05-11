85d23a43210499ad776e89738d2fafef6af0a1cb
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2015-05-11 11:03:04 +0200

    fixed type, styling, use other ctrl

--- www-develop/js/controller/searchMainCtrl.ts
+++ www-develop/js/controller/searchMainCtrl.ts
@@ -1,12 +1,19 @@
 module Controller {
     export class SearchMainCtrl {
         query:any;
+
+        activeItem = '';
+
         constructor(private $scope, private $location) {
             this.query = {};
             this.query.city = '';
             this.query.dateFrom =  '';
             this.query.dateTo = '';
             this.query.range = 3;
+        }
+
+        isActive(item) {
+            return this.activeItem == item;
         }
 
         static controllerId:string = "SearchMainCtrl";
