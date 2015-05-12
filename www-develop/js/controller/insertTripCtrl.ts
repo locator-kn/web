dd17f0ad0662a951c37bc2d5994dd13c6ba01945
DennisMoosmann <dennis.moosmann@gmx.de>
2015-05-12 11:03:14 +0200

    insert trip view new style

--- www-develop/js/controller/insertTripCtrl.ts
+++ www-develop/js/controller/insertTripCtrl.ts
@@ -1,5 +1,16 @@
 module Controller {
     export class InsertTripCtrl {
+
+        activeItem:string = '';
+
+        constructor(private $scope) {
+
+        }
+
+        isActive(item) {
+            return item == this.activeItem;
+        }
+
         static controllerId:string="InsertTripCtrl";
     }
 }
