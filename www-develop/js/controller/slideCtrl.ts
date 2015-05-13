1e97a007f830d111f7af798534d0291db89f2c2a
Michael Knoch <info@michaelknoch.de>
2015-05-13 12:01:25 +0200

    initial refactor

--- www-develop/js/controller/slideCtrl.ts
+++ www-develop/js/controller/slideCtrl.ts
@@ -1,14 +1,20 @@
 module Controller {
     export class SlideCtrl {
 
-        constructor($scope) {
-            $scope.slides = [
+        slides:string[];
+
+        constructor($scope, private $state) {
+            this.slides = [
                 './images/konstanz_background01.jpg',
                 './images/konstanz_background02.jpg',
                 './images/konstanz_background03.jpg'
                 //'https://ununsplash.imgix.net/photo-1429043794791-eb8f26f44081?fit=crop&fm=jpg&h=775&q=75&w=1050',
                 //'https://unsplash.imgix.net/photo-1428908799722-0a74e26ce7f6?fit=crop&fm=jpg&h=700&q=75&w=1050',
             ];
+        }
+
+        get showSlider() {
+            return this.$state.includes('welcome');
         }
 
         static controllerId:string = "SlideCtrl";
