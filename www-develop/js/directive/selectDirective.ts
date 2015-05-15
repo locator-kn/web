fcea8e2bcb6404c11b24c0ef8344a29294cd1814
Michael Knoch <info@michaelknoch.de>
2015-05-15 17:55:51 +0200

    reformat

--- www-develop/js/directive/selectDirective.ts
+++ www-develop/js/directive/selectDirective.ts
@@ -17,7 +17,7 @@ angular.module('starter').directive('select', function () {
                 console.info('Multiple selection');
 
                 $scope.select = function (value) {
-                    
+
                     $scope.selectedModel.push(value);
                     $scope.trigger();
                 }
@@ -28,10 +28,7 @@ angular.module('starter').directive('select', function () {
                     $scope.selectedModel = value;
                     $scope.trigger();
                 }
-
             }
-
-
         },
         template: '<div ng-if="!multiple" class="relative" ng-class="{open: opened}"><a ng-click="trigger()">{{selectedModel.title}}</a><ul class="sub"><li ng-click="select(mood)" ng-repeat="mood in values">{{mood.title}}</li></ul></div>' +
         '<div ng-if="multiple" class="relative" ng-class="{open: opened}"><a ng-repeat="item in selectedModel" ng-click="trigger()">{{item.title}}</a><ul class="sub"><li ng-click="select(mood)" ng-repeat="mood in values">{{mood.title}}</li></ul></div>'
