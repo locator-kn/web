c388a3682f1454e38fcd5cd11fb8ae85893be387
Michael Knoch <info@michaelknoch.de>
2015-05-15 18:09:49 +0200

    improve select directive

--- www-develop/js/directive/selectDirective.ts
+++ www-develop/js/directive/selectDirective.ts
@@ -31,7 +31,7 @@ angular.module('starter').directive('select', function () {
             }
         },
         template: '<div ng-if="!multiple" class="relative" ng-class="{open: opened}"><a ng-click="trigger()">{{selectedModel.title}}</a><ul class="sub"><li ng-click="select(mood)" ng-repeat="mood in values">{{mood.title}}</li></ul></div>' +
-        '<div ng-if="multiple" class="relative" ng-class="{open: opened}"><a ng-repeat="item in selectedModel" ng-click="trigger()">{{item.title}}</a><ul class="sub"><li ng-click="select(mood)" ng-repeat="mood in values">{{mood.title}}</li></ul></div>'
+        '<div ng-if="multiple" class="relative" ng-class="{open: opened}"><a ng-repeat="item in selectedModel" class="fullwidth" ng-click="trigger()">{{item.title}}</a><ul class="sub"><li ng-click="select(mood)" ng-repeat="mood in values">{{mood.title}}</li></ul></div>'
     }
 
 });
