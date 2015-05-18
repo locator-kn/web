4db6c59f5ed78f9a9ac9383a41170e9235a4a784
Timo Weiß <info@timo-weiss.com>
2015-05-18 15:49:24 +0200

    deactivate overlay

--- www-develop/js/controller/insertTripCtrl.ts
+++ www-develop/js/controller/insertTripCtrl.ts
@@ -30,7 +30,7 @@ module Controller {
             $scope.$on('mapentrySelected', (event, details)  => {
                 this.selectedPlaceDetails = details;
             });
-            $rootScope.overlay = true;
+            $rootScope.overlay = false;
         }
 
         isActive(item) {
