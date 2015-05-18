798fbf72676f7de5f7356d13fcbff4a04f8f9dd2
Timo Weiß <info@timo-weiss.com>
2015-05-18 12:52:58 +0200

    replace autocomplete directive

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -41,7 +41,7 @@
 //set to true if backend is running on localhost:3001
 var live = '<%= live %>';
 
-var app = angular.module('starter', ['locator.selection', 'cfp.hotkeys', 'ngDialog', 'angular-flexslider', 'smoothScroll', 'ui.router', 'pascalprecht.translate', 'emoji', 'base64', 'angularFileUpload', 'ngAnimate', 'ngAutocomplete'])
+var app = angular.module('starter', ['locator.selection', 'cfp.hotkeys', 'ngDialog', 'angular-flexslider', 'smoothScroll', 'ui.router', 'pascalprecht.translate', 'emoji', 'base64', 'angularFileUpload', 'ngAnimate', 'ngMapAutocomplete'])
 
     .constant('basePath', '<%= basePath %>')
 
