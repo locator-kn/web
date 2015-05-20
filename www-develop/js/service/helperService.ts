94407fcebb96288c2a772a9cd2344a1214cb2d02
Michael Knoch <info@michaelknoch.de>
2015-05-20 14:36:48 +0200

    implement context save

--- www-develop/js/service/helperService.ts
+++ www-develop/js/service/helperService.ts
@@ -1,17 +1,26 @@
 module Service {
     export class HelperService {
 
-        constructor(private $http, private basePath) {
+        constructor(private $http, private basePath, private $location) {
 
         }
 
         getMoodQuery(moods) {
             var moodQuery = [];
-            moods.forEach(function(entry) {
+            moods.forEach(function (entry) {
                 moodQuery.push(entry.query_name);
             });
 
             return moodQuery.join('.');
+        }
+
+        saveContext() {
+            localStorage.setItem('location', this.$location.$$absUrl);
+        }
+
+        getContext() {
+            console.info('Get Context: ' + localStorage.getItem('location'));
+            return localStorage.getItem('location');
         }
 
         static serviceId:string = "HelperService";
