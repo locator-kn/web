56742364b36c533047f1d312c5d6463b75463f29
Michael Knoch <info@michaelknoch.de>
2015-05-22 16:58:14 +0200

    implement conversations

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -2,9 +2,19 @@ module Controller {
     export class MessengerCtrl {
 
         overlay:boolean;
+        conversations;
 
         constructor(private MessengerService) {
+            this.getConversations();
 
+        }
+
+        getConversations() {
+            this.MessengerService.getConversations()
+                .then(result => {
+                    this.conversations = result.data;
+                    console.info(this.conversations);
+                });
         }
 
         static controllerId:string = "MessengerCtrl";
