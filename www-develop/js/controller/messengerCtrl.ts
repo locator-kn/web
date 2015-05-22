39c5077b38e8beb69f1629c139ec9ba88b365d06
Michael Knoch <info@michaelknoch.de>
2015-05-22 19:17:17 +0200

    implement messenger

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -3,18 +3,36 @@ module Controller {
 
         overlay:boolean;
         conversations;
+        selectedConversation;
+        messages;
 
-        constructor(private MessengerService) {
+        constructor(private MessengerService, private UserService, private $rootScope) {
             this.getConversations();
-
+            this.getConversation();
         }
 
+        // conversationlist
         getConversations() {
             this.MessengerService.getConversations()
                 .then(result => {
                     this.conversations = result.data;
-                    console.info(this.conversations);
                 });
+        }
+
+        // one single conversation
+        getConversation() {
+            this.MessengerService.getConversation()
+                .then(result => {
+
+                    this.messages = result.data;
+                    console.info(this.messages);
+
+                });
+        }
+
+        // select a conversation to show message content
+        select(conversation) {
+            this.selectedConversation = conversation;
         }
 
         static controllerId:string = "MessengerCtrl";
