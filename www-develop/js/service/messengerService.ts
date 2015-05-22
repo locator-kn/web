41df9446d96df8c677d17bf9a25c9741c23e1f52
Michael Knoch <info@michaelknoch.de>
2015-05-22 16:12:55 +0200

    implement conversations

--- www-develop/js/service/messengerService.ts
+++ www-develop/js/service/messengerService.ts
@@ -1,14 +1,65 @@
-
 module Service {
     export class MessengerService {
 
-        citiesWithTrips = [];
 
-        constructor(private $http) {
+        constructor(private $http, private $q) {
 
         }
 
 
-        static serviceId:string = "MessengerService";
+        getConversations() {
+            return this.$q(function (resolve) {
+                    setTimeout(function () {
+                        resolve(
+                            {
+                                data: [
+                                    {
+                                        "_id": "1",
+                                        "user_1": 'dewdewdewdwed',
+                                        "user_2": 'dewewfewf',
+                                        "user_1_read": true,
+                                        "user_2_read": true,
+                                        "lastMessage": 'Ich bin Steffanie'
+                                    }
+                                ]
+                            });
+                    }, 500);
+                }
+            );
+        }
+
+        getConversation() {
+            return this.$q(function (resolve) {
+                    setTimeout(function () {
+                        resolve(
+                            {
+                                data: [
+                                    {
+                                        "_id": "1",
+                                        "sender": 'userid',
+                                        "timestamp" : 'erster Timestamp',
+                                        "message": 'Ich bin Steffanie'
+                                    },
+                                    {
+                                        "_id": "2",
+                                        "sender": 'userid',
+                                        "timestamp" : 'zweiter Timestamp',
+                                        "message": 'Ich bin Steffanie'
+                                    },
+                                    {
+                                        "_id": "3",
+                                        "sender": 'userid',
+                                        "timestamp" : 'später',
+                                        "message": 'Ich bin Steffanie'
+                                    }
+                                ]
+                            });
+                    }, 500);
+                }
+            );
+        }
+
+        static
+            serviceId:string = "MessengerService";
     }
 }
