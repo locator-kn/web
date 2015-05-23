6ad11323259a44c6fd34763bdd2c458fc5320768
Timo Weiß <info@timo-weiss.com>
2015-05-23 15:47:23 +0200

    refactoring search

--- www-develop/js/service/searchService.ts
+++ www-develop/js/service/searchService.ts
@@ -3,7 +3,7 @@ module Service {
 
         citiesWithTrips = [];
 
-        constructor(private $http, private $location, private basePath, private DataService) {
+        constructor(private $http, private $location, private basePath, private DataService, private lodash, private $q) {
 
         }
 
@@ -12,19 +12,32 @@ module Service {
         }
 
         getTripsByQuery(searchQuery) {
+            // create a copy by value
+            var sq = this.lodash.cloneDeep(searchQuery);
 
-            //delete searchQuery.city;
-            var query = 'http://locator.in.htwg-konstanz.de/api/v1/trips';
-            query += this.getQueryParamString(searchQuery);
-            console.info(query);
-            return this.$http({
-                url: query,
-                method: "GET"
-            })
+            var query = 'http://locator.in.htwg-konstanz.de/api/v1/trips/search';
+            return this.getCityId(sq.city).then(cityid => {
+                // delete city from query since it is part of the path
+                delete sq.city;
+
+                // returning a promise inside a promise will make the outside promise resolving if inside is resolved.
+                return this.$http({
+                    url: query + '/' + cityid,
+                    params: sq,
+                    method: 'GET'
+                });
+            });
         }
 
+
+        /**
+         *  TODO: remove if not used anymore. Calling this.$http with key 'params' will do the work for you
+         * @param searchQuery
+         * @returns {*}
+         */
         getQueryParamString(searchQuery) {
             var cityId = this.getCityId(searchQuery.city);
+
             var queryString = this.$location.url();
             queryString = queryString.replace('?city='+searchQuery.city, '/'+ cityId);
             queryString = queryString.replace('&moods=','.');
@@ -33,19 +46,20 @@ module Service {
         }
 
         getCityId(city) {
-            var id;
+            var promise = this.$q((resolve, reject) => {
+                this.DataService.getAvailableCities()
+                    .then(result => {
+                        this.citiesWithTrips = result.data;
+                        var cityObject = this.lodash.findWhere(this.citiesWithTrips, {title: city});
+                        if(cityObject.place_id) {
+                            return resolve(cityObject.id);
+                        }
+                        reject({msg: 'not found'});
+                    });
 
-            this.DataService.getAvailableCities()
-                .then(result => {
-                    this.citiesWithTrips = result.data;
-                });
-
-            this.citiesWithTrips.forEach(function(entry) {
-                if(city == entry.title) {
-                    id = entry.id;
-                }
             });
-            return id;
+
+            return promise;
         }
 
 
