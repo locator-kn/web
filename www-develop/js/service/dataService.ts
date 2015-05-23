7a9f130a6e62598a18d28d18a1b6620f8fabfefa
Timo Weiß <info@timo-weiss.com>
2015-05-23 16:21:51 +0200

    add cache-fn sample in dataService

--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -3,8 +3,9 @@ module Service {
 
         checkinDate;
         checkoutDate;
+        private citiesCache:Object = {};
 
-        constructor(private $http, private basePath) {
+        constructor(private $http, private basePath, private $q) {
             this.checkinDate = new Date();
             this.checkoutDate = moment(this.checkinDate).add(3, 'days').toDate();
         }
@@ -29,7 +30,24 @@ module Service {
         }
 
         getAvailableCities() {
-            return this.$http.get(this.basePath + '/data/cities');
+            return this.getCachePromise(this.basePath + '/data/cities', 'citiesCache');
+        }
+
+        getCachePromise(requestUri: string, cacheObjName) {
+            return this.$q((resolve, reject) => {
+
+                if (!this[cacheObjName].hasOwnProperty('status')) {
+                    console.log('do the request')
+                    this.$http.get(requestUri).then(response => {
+                        // set/update cache
+                        this[cacheObjName] = response;
+                        resolve(response);
+                    }).catch(reject);
+                } else {
+                    console.log('serve cities from cache');
+                    resolve(this[cacheObjName]);
+                }
+            });
         }
 
         static serviceId:string = "DataService";
