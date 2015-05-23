a755bd18f17621405395113b986b92bd6e152be0
Timo Weiß <info@timo-weiss.com>
2015-05-23 14:39:04 +0200

    fix id -> _id

--- www-develop/js/service/insertTripService.ts
+++ www-develop/js/service/insertTripService.ts
@@ -7,10 +7,10 @@ module Service {
         saveTrip(newTrip, documentMetaData) {
             if(documentMetaData._id && documentMetaData._rev ) {
                 // extend new trip with meta data id and rev
-                newTrip._id = documentMetaData.id;
+                newTrip._id = documentMetaData._id;
                 newTrip._rev = documentMetaData._rev;
 
-                return this.$http.put(this.basePath + '/trips/' + documentMetaData.id, newTrip);
+                return this.$http.put(this.basePath + '/trips/' + documentMetaData._id, newTrip);
             }
             return this.$http.post(this.basePath + '/trips', newTrip);
         }
