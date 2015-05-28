92f215ef388aad06ce7964953c17d7fe057a0014
Timo Weiß <info@timo-weiss.com>
2015-05-28 19:25:27 +0200

    add getSocket and onEvent

--- www-develop/js/service/socketService.ts
+++ www-develop/js/service/socketService.ts
@@ -3,10 +3,34 @@ module Service {
     var io;
     export class SocketService {
 
-        socket;
+        socket = null;
 
-        constructor(private $http, private basePath, private $rootScope, private socketFactory) {
+        constructor(private $http, private $q, private basePath, private $rootScope, private socketFactory) {
             this.socketInit();
+        }
+
+        getSocket() {
+            return this.$q((resolve, reject) => {
+               if(this.socket) {
+                   resolve(this.socket);
+               } else {
+                   this.$http.get('http://localhost:3002/api/v1/connect/me')
+                       .error(err => {
+                           reject(err);
+                       })
+                       .then(response => {
+                           var myIoSocket = io.connect(':3002' + response.data.namespace);
+                           this.socket = this.socketFactory({ioSocket: myIoSocket});
+                           resolve(this.socket)
+                       });
+               }
+            });
+        }
+
+        onEvent(event, fn) {
+            this.getSocket().then(socket => {
+                socket.on(event, fn);
+            })
         }
 
         socketInit() {
