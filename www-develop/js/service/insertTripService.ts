015f956b3417b8c97e23d2c44c45138b4d392044
Timo Weiß <info@timo-weiss.com>
2015-06-01 15:32:26 +0200

    delete key

--- www-develop/js/service/insertTripService.ts
+++ www-develop/js/service/insertTripService.ts
@@ -18,6 +18,10 @@ module Service {
             return this.$http.post(this.basePath + '/trips', newTrip);
         }
         uploadImage(formData, file) {
+
+            // To be sure keys don't exist
+            delete formData._id;
+            delete formData._rev;
             return this.Upload.upload({
                 url: this.basePath + '/trips/image',
                 fields: formData,
