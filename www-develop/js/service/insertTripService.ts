b30f573e10d8915b098819b3714af4e4ee1beca5
Timo Weiß <info@timo-weiss.com>
2015-06-01 15:28:40 +0200

    delete _id _rev if post

--- www-develop/js/service/insertTripService.ts
+++ www-develop/js/service/insertTripService.ts
@@ -12,6 +12,9 @@ module Service {
 
                 return this.$http.put(this.basePath + '/trips/' + documentMetaData._id, newTrip);
             }
+            // To be sure keys don't exist
+            delete documentMetaData._id;
+            delete documentMetaData._rev;
             return this.$http.post(this.basePath + '/trips', newTrip);
         }
         uploadImage(formData, file) {
