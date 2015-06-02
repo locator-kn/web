84ba7607c86381097deafb2a019311203678712e
Timo Weiß <info@timo-weiss.com>
2015-06-02 11:57:58 +0200

    impl. cache using default conf for static data

--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -4,9 +4,19 @@ module Service {
         checkinDate;
         checkoutDate;
 
-        constructor(private $http, private basePath) {
+        dataCitiesCache;
+        dataAccommodationsCache;
+        dataMoodsCache;
+
+        constructor(private $http, private basePath, private CacheFactory) {
             this.checkinDate = new Date();
             this.checkoutDate = moment(this.checkinDate).add(3, 'days').toDate();
+
+
+            this.dataCitiesCache = CacheFactory.createCache('dataCities');
+            this.dataAccommodationsCache = CacheFactory.createCache('dataAccommodations');
+            this.dataMoodsCache = CacheFactory.createCache('dataMoods');
+
         }
 
         getDates() {
@@ -17,19 +27,19 @@ module Service {
         }
 
         getCities() {
-            return this.$http.get(this.basePath + '/data/cities');
+            return this.$http.get(this.basePath + '/data/cities', {cache: this.dataCitiesCache});
         }
 
         getAccomodations() {
-            return this.$http.get(this.basePath + '/data/accommodations');
+            return this.$http.get(this.basePath + '/data/accommodations', {cache: this.dataAccommodationsCache});
         }
 
         getMoods() {
-            return this.$http.get(this.basePath + '/data/moods');
+            return this.$http.get(this.basePath + '/data/moods', {cache: this.dataMoodsCache});
         }
 
         getAvailableCities() {
-            return this.$http.get(this.basePath + '/data/cities');
+            return this.$http.get(this.basePath + '/data/cities', {cache: this.dataCitiesCache});
         }
 
         static serviceId:string = "DataService";
