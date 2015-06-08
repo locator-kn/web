ff29e2507401a74e16b06924d1e589a982daaf45
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2015-06-08 16:49:38 +0200

    fixed date format

--- www-develop/js/controller/tripCtrl.ts
+++ www-develop/js/controller/tripCtrl.ts
@@ -4,11 +4,13 @@ module Controller {
     export class TripCtrl {
 
         trip:any = {};
-
+        start_date;
         constructor(private $scope, private $stateParams, private SearchService) {
             this.SearchService.getTripById(this.$stateParams.tripId)
                 .then(result => {
                     this.trip = result.data[0];
+                    this.trip.start_date = moment(new Date(this.trip.start_date)).format('L');
+                    this.trip.end_date = moment(new Date(this.trip.end_date)).format('L');
                 });
         }
 
