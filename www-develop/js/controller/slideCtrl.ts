2529080ab2d49e5bc7036e5ab161e461fc06b7f2
Michael Knoch <info@michaelknoch.de>
2015-06-14 20:33:26 +0200

    implement getrandomimage

--- www-develop/js/controller/slideCtrl.ts
+++ www-develop/js/controller/slideCtrl.ts
@@ -2,6 +2,8 @@ module Controller {
     export class SlideCtrl {
 
         slides:string[];
+        randomLeft:string;
+        randomRight:string;
 
         constructor($scope, private $state) {
             this.slides = [
@@ -10,9 +12,16 @@ module Controller {
                 './images/konstanz_background03.jpg',
                 './images/konstanz_background05.jpg',
                 './images/konstanz_background08.jpg'
-                //'https://ununsplash.imgix.net/photo-1429043794791-eb8f26f44081?fit=crop&fm=jpg&h=775&q=75&w=1050',
-                //'https://unsplash.imgix.net/photo-1428908799722-0a74e26ce7f6?fit=crop&fm=jpg&h=700&q=75&w=1050',
             ];
+
+            this.randomLeft = this.getRandomImage();
+            do {
+                this.randomRight = this.getRandomImage();
+            } while (this.randomLeft === this.randomRight);
+        }
+
+        getRandomImage() {
+            return this.slides[Math.floor((Math.random() * this.slides.length))];
         }
 
         get showSlider() {
