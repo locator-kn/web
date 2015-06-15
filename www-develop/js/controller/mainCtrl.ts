e05ea7d44af35eede7eef5e6564207b983c3c257
Timo Weiß <info@timo-weiss.com>
2015-06-15 15:18:16 +0200

    add openElement, add eventListenener

--- www-develop/js/controller/mainCtrl.ts
+++ www-develop/js/controller/mainCtrl.ts
@@ -2,10 +2,17 @@ module Controller {
     export class MainCtrl {
 
         overlay:boolean;
+        openElement:string = 'user';
 
         constructor(private $rootScope, private $location) {
             this.$rootScope.overlay = false;
+            this.$rootScope.openElement = this.openElement;
 
+
+            $rootScope.$on('newPopoverSelected', (e, clickValue)=> {
+                debugger
+                this.openElement = this.$rootScope.openElement = clickValue;
+            });
 
             this.$rootScope.$on("$locationChangeStart", () => {
                 $rootScope.slider = $location.$$path === '/welcome';
