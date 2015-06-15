dabb1c2c4a380f2c704d67ca9edc3002679c8d49
Michael Knoch <info@michaelknoch.de>
2015-06-15 14:51:25 +0200

    tripctrl

--- www-develop/js/controller/tripCtrl.ts
+++ www-develop/js/controller/tripCtrl.ts
@@ -1,21 +1,22 @@
 module Controller {
 
-
     export class TripCtrl {
 
         trip:any = {};
         start_date;
-        constructor(private $rootScope, private $stateParams, private SearchService) {
-            this.$rootScope.showSearchButton = true;
-            this.$rootScope.showCreateButton = true;
+        trips:any;
 
-            this.SearchService.getTripById(this.$stateParams.tripId)
-                .then(result => {
-                    this.trip = result.data[0];
-                    this.trip.start_date = moment(new Date(this.trip.start_date)).format('L');
-                    this.trip.end_date = moment(new Date(this.trip.end_date)).format('L');
-                });
+        constructor(private $rootScope, private $stateParams, private SearchService, private TripService) {
+            this.getMyTrips();
         }
+
+        getMyTrips() {
+            this.TripService.getMyTrips().then(result => {
+                this.trips = result.data;
+                console.info(this.trips);
+            })
+        }
+
 
         static controllerId:string = "TripCtrl";
     }
