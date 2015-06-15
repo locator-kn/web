9e38c028885b3788f549dc02708e6bbb3ada641c
Michael Knoch <info@michaelknoch.de>
2015-06-15 17:04:41 +0200

    refactor

--- www-develop/js/controller/tripCtrl.ts
+++ www-develop/js/controller/tripCtrl.ts
@@ -8,6 +8,16 @@ module Controller {
 
         constructor(private $rootScope, private $stateParams, private SearchService, private TripService) {
             this.getMyTrips();
+
+            this.$rootScope.showSearchButton = true;
+            this.$rootScope.showCreateButton = true;
+
+            this.SearchService.getTripById(this.$stateParams.tripId)
+                .then(result => {
+                    this.trip = result.data[0];
+                    this.trip.start_date = moment(new Date(this.trip.start_date)).format('L');
+                    this.trip.end_date = moment(new Date(this.trip.end_date)).format('L');
+                });
         }
 
         getMyTrips() {
