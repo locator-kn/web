a37f66e8cd770a1c0827ba64d18652cfd5cfa307
T1m1 <Ruprecht.t@gmx.de>
2015-06-15 12:50:09 +0200

    add array of moods

--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -9,6 +9,80 @@ module Service {
         dataMoodsCache;
         dayCache;
 
+        availableMoods = [{
+            query_name: 'buddytrip',
+            title: 'buddytrip',
+            icon: 'buddytripy.png',
+            icon_grey: 'buddytripy.png',
+            description: ''
+        }, {
+            query_name: 'girls_on_tour',
+            title: 'girls on tour',
+            icon: 'girls_on_tour.png',
+            icon_grey: 'girls_on_tour_grey.png',
+            description: ''
+        }, {
+            query_name: 'hallihalli_drecksau',
+            title: 'halligalli drecksau',
+            icon: 'hallihalli_drecksau.png',
+            icon_grey: 'hallihalli_drecksau_ausgegraut.png',
+            description: ''
+        }, {
+            query_name: 'muskelkater',
+            title: 'muskelkater',
+            icon: 'muskelkater.png',
+            icon_grey: 'muskelkater_grey.png',
+            description: ''
+        }, {
+            query_name: 'sturm_der_liebe',
+            title: 'strum der liebe',
+            icon: 'sturm_der_liebe.png',
+            icon_grey: 'sturm_der_liebe_grey.png',
+            description: ''
+        }, {
+            query_name: 'gruener_gehts_nicht',
+            title: 'grüner gehts nicht',
+            icon: 'gruener_gehts_nicht.png',
+            icon_grey: 'gruener_gehts_nicht_grey.png',
+            description: ''
+        }, {
+            query_name: 'haste_nicht_gesehen',
+            title: 'haste nicht gesehen',
+            icon: 'haste_nicht_gesehen.png',
+            icon: 'haste_nicht_gesehen_grey.png',
+            description: ''
+        }, {
+            query_name: 'singles_unter_sich',
+            title: 'singles unter sich',
+            icon: 'singles_unter_sich.png',
+            icon_grey: 'singles_unter_sich_grey.png',
+            description: ''
+        }, {
+            query_name: 'family_fun',
+            title: 'family fun',
+            icon: 'family_fun.png',
+            icon_grey: 'family_fun_grey.png',
+            description: ''
+        }, {
+            query_name: 'kultur_und_siteseeing',
+            title: 'kultur und siteseeing',
+            icon: 'kultur_und_siteseeing.png',
+            icon_grey: 'kultur_und_siteseeing_grey.png',
+            description: ''
+        }, {
+            query_name: 'leckermaeulchen',
+            title: 'leckermäulchen',
+            icon: 'leckermaeulchen.png',
+            icon_grey: 'leckermaeulchen_grey.png',
+            description: ''
+        }, {
+            query_name: 'entspannung_pur',
+            title: 'entspannnung pur',
+            icon: 'entspannung_pur.png',
+            icon_grey: 'entspannung_pur_grey.png',
+            description: ''
+        }];
+
         constructor(private $http, private basePath, private CacheFactory, private $q) {
             this.checkinDate = new Date();
             this.checkoutDate = moment(this.checkinDate).add(3, 'days').toDate();
@@ -37,7 +111,7 @@ module Service {
         }
 
         getMoods() {
-            return this.$http.get(this.basePath + '/data/moods', {cache: this.dataMoodsCache});
+            return this.availableMoods;
         }
 
         getAvailableCities() {
