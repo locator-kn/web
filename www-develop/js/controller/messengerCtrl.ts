102245d1e8d814e4a90318926c0bea4519496ada
Timo Weiß <info@timo-weiss.com>
2015-06-21 21:19:18 +0200

    add experimental delay for acks

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -111,7 +111,9 @@ module Controller {
 
         emitAck(from, conversation_id) {
             console.log('send ack for received message', {from: this.$rootScope.userID, opponent: from, conversation_id: conversation_id});
-            this.SocketService.emit('message_ack', {from: this.$rootScope.userID, opponent: from, conversation_id: conversation_id});
+            setTimeout(() => {
+                this.SocketService.emit('message_ack', {from: this.$rootScope.userID, opponent: from, conversation_id: conversation_id});
+            }, 200);
             this.conversationsHash[conversation_id][this.$rootScope.userID + '_read'] = true;
         }
 
