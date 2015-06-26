1cd2ba27da99a03a8364a67e7e129d88ca11c1af
Michael Knoch <info@michaelknoch.de>
2015-06-26 18:41:27 +0200

    fix objects by _id

--- www-develop/js/service/helperService.ts
+++ www-develop/js/service/helperService.ts
@@ -36,6 +36,15 @@ module Service {
             return this.lodash.findWhere(array, {'query_name': queryName});
         }
 
+        getObjectById(array, id) {
+            return this.lodash.findWhere(array, {'id': id.toString()});
+        }
+
+        getObjectBy_Id(array, _id) {
+            debugger;
+            return this.lodash.findWhere(array, {'_id': _id});
+        }
+
         getCityByTitle(cities, title) {
             return this.lodash.findWhere(cities, {'title': title});
         }
