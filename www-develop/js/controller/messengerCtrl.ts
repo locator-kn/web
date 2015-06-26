fde8c04fc2966e64116c8c425f4afaca38e86d4c
Timo Weiß <info@timo-weiss.com>
2015-06-26 22:01:09 +0200

    try $scope to make sure event listener gets destroyed

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -18,7 +18,7 @@ module Controller {
 
         emojis = [":smile:", ":blush:", ":kissing_heart:", ":hear_no_evil:", ":speak_no_evil:", ":see_no_evil:"];
 
-        constructor(private $filter, private $sce, private MessengerService, private $state, private UserService, private $rootScope, private SocketService, private CacheFactory, private basePathRealtime) {
+        constructor(private $filter, private $scope, private $sce, private MessengerService, private $state, private UserService, private $rootScope, private SocketService, private CacheFactory, private basePathRealtime) {
 
 
             this.getConversations();
@@ -50,7 +50,7 @@ module Controller {
 
         registerSocketEvent() {
             //this.SocketService.offEvent('new_message');
-            this.$rootScope.$on('new_message', (evt, newMessage) => {
+            this.$scope.$on('new_message', (evt, newMessage) => {
                 console.log('neWmEssage');
                 if(this.$state.params.opponentId === newMessage.conversation_id ) {
                     if (this.selectedConversation._id === newMessage.conversation_id) {
