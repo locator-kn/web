c9ab69dc185c94f670cb265d93c7afacc2402833
Michael Knoch <info@michaelknoch.de>
2015-06-27 14:00:51 +0200

    add footer

--- www-develop/js/controller/editTripCtrl.ts
+++ www-develop/js/controller/editTripCtrl.ts
@@ -221,7 +221,7 @@ module Controller {
 
 
         dateValidation() {
-            
+
             if (this.tripMeta.start_date === "Invalid date" || this.tripMeta.end_date === "Invalid date") {
                 this.dateValid = false;
             } else if (this.tripMeta.start_date === "" || this.tripMeta.end_date === "") {
