11c7ceae8597a9268a7cf3dc0f6d88b8ff9e99c4
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-06-30 09:29:52 +0200

    change service name

--- www-develop/js/controller/locationViewCtrl.ts
+++ www-develop/js/controller/locationViewCtrl.ts
@@ -5,10 +5,10 @@ module Controller {
         locationId:any;
         location:any;
 
-        constructor(private $scope, private $stateParams, private LocationServce) {
+        constructor(private $scope, private $stateParams, private LocationSerivce) {
             this.locationId = $stateParams.locationId;
 
-            this.LocationServce.getLocationById(this.locationId)
+            this.LocationSerivce.getLocationById(this.locationId)
                 .then(result => {
                     this.location = result.data;
                     console.log(this.location);
