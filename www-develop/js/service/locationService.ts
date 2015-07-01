f449f893d67882ffc3724488a5bb3e50e8e2648e
Michael Knoch <info@michaelknoch.de>
2015-07-01 12:37:34 +0200

    fix location images

--- www-develop/js/service/locationService.ts
+++ www-develop/js/service/locationService.ts
@@ -6,14 +6,25 @@ module Service {
 
         uploadImage(formData, file) {
 
-            // To be sure keys don't exist
+            var id = formData._id;
             delete formData._id;
             delete formData._rev;
-            return this.Upload.upload({
-                url: this.basePath + '/users/my/locations/picture',
-                fields: formData,
-                file: file
-            });
+
+            if (!id) {
+                return this.Upload.upload({
+                    url: this.basePath + '/users/my/locations/picture',
+                    fields: formData,
+                    file: file
+                });
+            } else {
+                return this.Upload.upload({
+
+                url: this.basePath + '/locations/' + id + '/picture',
+                    fields: formData,
+                    file: file
+                });
+            }
+
         }
 
         saveLocation(location, id?:string) {
