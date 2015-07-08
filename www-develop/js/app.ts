5574eabe47253935f015e69eee68c8fcea272a0f
Michael Knoch <info@michaelknoch.de>
2015-07-08 16:46:29 +0200

    slideshow false

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -303,7 +303,7 @@ var app = angular.module('locator', deps)
 
     .directive('imgTriplist', () => {
         var tmpl = [
-            '<flex-slider class="static" control-nav="false" direction-nav="true" animation="fade" animation-loop="true" slide="s in slides"><li>',
+            '<flex-slider class="static" control-nav="false" direction-nav="true" animation="fade" animation-loop="true" slideshow="false" slide="s in slides"><li>',
             '<div class="header-image" style="background-image: url({{s}});"></div>',
             '</li></flex-slider>'
         ];
