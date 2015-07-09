bbfad230ef745db306a5ba8809e07a106f9e5fb6
Timo Weiß <info@timo-weiss.com>
2015-07-10 00:33:53 +0200

    cleanup

--- www-develop/js/controller/contextCtrl.ts
+++ www-develop/js/controller/contextCtrl.ts
@@ -5,7 +5,7 @@ module Controller {
 
         context:string;
 
-        constructor(private $scope, private basePath, private HelperService, private $location, private $window, private $state) {
+        constructor(private HelperService, private $state) {
             //debugger;
             var state = this.HelperService.getContext();
             this.$state.go(state.name, state.params);
