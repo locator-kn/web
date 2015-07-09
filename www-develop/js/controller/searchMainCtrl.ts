ca8496b50c3e3046c8410fe2bec6e6cd03569805
Timo Weiß <info@timo-weiss.com>
2015-07-10 00:40:48 +0200

    codestyle

--- www-develop/js/controller/searchMainCtrl.ts
+++ www-develop/js/controller/searchMainCtrl.ts
@@ -1,10 +1,11 @@
 module Controller {
     export class SearchMainCtrl {
         query:any;
+
         constructor() {
             this.query = {};
             this.query.city = '';
-            this.query.dateFrom =  '';
+            this.query.dateFrom = '';
             this.query.dateTo = '';
             this.query.range = 3;
         }
