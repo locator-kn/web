102cc1526106334e975435fbc4ca257fe61032be
Michael Knoch <info@michaelknoch.de>
2015-07-09 17:30:08 +0200

    init

---
+++ www-develop/js/service/feedbackService.ts
@@ -0,0 +1,12 @@
+module Service {
+    export class FeedbackService {
+
+        constructor(private $http, private basePath) {
+
+        }
+
+
+
+        static serviceId:string = "FeedbackService";
+    }
+}
