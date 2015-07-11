be27aac439c8a7683bcae516ef56af94c11e2fa4
Timo Weiß <info@timo-weiss.com>
2015-07-11 17:52:26 +0200

    impl getter

--- www-develop/js/controller/errorCtrl.ts
+++ www-develop/js/controller/errorCtrl.ts
@@ -2,9 +2,16 @@ module Controller {
 
     export class ErrorCtrl {
         params:any;
+        currentError:any;
+
         static $inject = ['$state', 'ErrorService'];
         constructor(private $state, private ErrorService) {
             this.params = this.$state.params;
+
+            if(this.params.t) {
+                this.currentError = this.ErrorService.getError(this.params.t);
+            }
+
         }
 
         static controllerId:string = "ErrorCtrl";
