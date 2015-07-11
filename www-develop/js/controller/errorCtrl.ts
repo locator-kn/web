cd042bd389414dae3accdb4300bd43be16048e19
Timo Weiß <info@timo-weiss.com>
2015-07-11 16:25:31 +0200

    init error ctrl

---
+++ www-develop/js/controller/errorCtrl.ts
@@ -0,0 +1,11 @@
+module Controller {
+
+    export class ErrorCtrl {
+
+        static $inject = [];
+        constructor() {
+        }
+
+        static controllerId:string = "ErrorCtrl";
+    }
+}
