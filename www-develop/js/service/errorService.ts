6b5084e80bdf044478874e0ed17a315a3194f7df
Timo Weiß <info@timo-weiss.com>
2015-07-11 17:42:12 +0200

    add setError

--- www-develop/js/service/errorService.ts
+++ www-develop/js/service/errorService.ts
@@ -1,9 +1,15 @@
 module Service {
     export class ErrorService {
 
+        errorHashMap:any = {};
+
         static $inject = [];
         constructor() {
 
+        }
+
+        setError(timestamp, error) {
+            this.errorHashMap[timestamp] = error;
         }
 
         static serviceId:string = "ErrorService";
