f3bf7899f8ee0864626b33597e595fea4f4ee669
Michael Knoch <info@michaelknoch.de>
2015-07-13 20:23:19 +0200

    hover tests

--- www-develop/js/controller/editTripCtrl.ts
+++ www-develop/js/controller/editTripCtrl.ts
@@ -55,6 +55,11 @@ module Controller {
 
         constructor(private UtilityService, private smoothScroll, private $q, private lodash, private $scope, private $rootScope, private $state, private $location, private InsertTripService, private TripService, private LocationService, private DataService, private HelperService, private ErrorService) {
 
+            angular.element('.hover').bind('touchstart touchend', function (e) {
+                console.info(e);
+                console.info('hover');
+            });
+
             if ($state.current.name === 'insertTrip') {
                 this.$rootScope.breadcrumb = 'Trip einstellen';
             }
