4218fd21b8b2885b3b90f49cf19d32c05d608202
Michael Knoch <info@michaelknoch.de>
2015-07-13 10:26:45 +0200

    testing generic message possibilities

--- www-develop/js/service/utilityService.ts
+++ www-develop/js/service/utilityService.ts
@@ -1,8 +1,9 @@
 module Service {
     export class UtilityService {
 
-        constructor() {
+        static $inject = ['ngDialog'];
 
+        constructor(private ngDialog) {
         }
 
 
@@ -13,9 +14,9 @@ module Service {
         // Borrowed from: http://davidwalsh.name/javascript-debounce-function
         debounce(func:any, wait:number, immediate:boolean) {
             var timeout;
-            return function() {
+            return function () {
                 var context = this, args = arguments;
-                var later = function() {
+                var later = function () {
                     timeout = null;
                     if (!immediate) func.apply(context, args);
                 };
@@ -28,17 +29,52 @@ module Service {
 
         softDebounce(func:any, wait:number, immediate:boolean) {
             var timeout;
-            return function() {
+            return function () {
                 var context = this, args = arguments;
-                var later = function() {
+                var later = function () {
                     timeout = null;
                     if (!immediate) func.apply(context, args);
                 };
                 var callNow = immediate && !timeout;
-                if(!timeout) timeout = setTimeout(later, wait);
+                if (!timeout) timeout = setTimeout(later, wait);
                 if (callNow) func.apply(context, args);
             };
         }
+
+        errorMsg(title, msg) {
+            var modal = this.ngDialog.open({
+                controller: ($scope) => {
+                    $scope.close = () => {
+                        modal.close();
+                    }
+                },
+                template: '<h3>' + title + '</h3>' +
+                '<p>' + msg + '</p>' +
+                '<a class="pointer" ng-click="close()">Ok</a>',
+                plain: true
+            });
+        }
+
+        genericMsg(title, msg, buttonTitle, buttonFunction) {
+
+            var modal = this.ngDialog.open({
+                controller: ($scope) => {
+
+                    $scope.ttle = title;
+                    $scope.msg = msg;
+                    $scope.buttonTitle = buttonTitle;
+                    $scope.buttonFunction = buttonFunction;
+
+                    $scope.close = () => {
+                        modal.close();
+                    }
+                },
+                template: "/templates/modal/genericMessage.html",
+                overlay: false
+            });
+
+        }
+
 
         static serviceId:string = "UtilityService";
     }
