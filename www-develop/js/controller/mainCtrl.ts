c8cfbc6e6abd2099bc5a3e4c91e713f7fd0a7d1f
Michael Knoch <info@michaelknoch.de>
2015-07-17 14:24:16 +0200

    mobile test

--- www-develop/js/controller/mainCtrl.ts
+++ www-develop/js/controller/mainCtrl.ts
@@ -21,9 +21,11 @@ module Controller {
 
         forgotPassword:boolean = false;
 
-        static $inject = ['$state', '$timeout', '$element', 'UserService', 'hotkeys', '$scope', '$rootScope', '$location', '$window'];
+        static $inject = ['UtilityService','$state', '$timeout', '$element', 'UserService', 'hotkeys', '$scope', '$rootScope', '$location', '$window'];
 
-        constructor(private $state, private $timeout, private $element, private UserService, private hotkeys, private $scope, private $rootScope, private $location, private $window) {
+        constructor(private UtilityService, private $state, private $timeout, private $element, private UserService, private hotkeys, private $scope, private $rootScope, private $location, private $window) {
+
+            console.info(this.UtilityService.isMobile());
 
             this.$rootScope.overlay = false;
             this.$rootScope.openElement = this.openElement;
