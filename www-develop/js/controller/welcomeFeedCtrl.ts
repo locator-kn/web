fe3699e765cc49f5886caff24d12571461e781de
Timo Weiß <info@timo-weiss.com>
2015-08-11 20:19:39 +0200

    add loadMoreLocations and decorateLocationsWithUser

--- www-develop/js/controller/welcomeFeedCtrl.ts
+++ www-develop/js/controller/welcomeFeedCtrl.ts
@@ -15,21 +15,30 @@ module Controller {
 
             this.LocationService.getLatestLocations(6, 0)
                 .then(result => {
-
-
-                    result.data.forEach((item:any) => {
-
-                        item.city.title = item.city.title.split(',')[0];
-                        this.UserService.getUser(item.userid).then(result => {
-                            item.user = result.data;
-                        });
-
-                    });
-
+                    this.decorateLocationsWithUser(result.data);
                     this.latestLocations = result.data;
 
 
                 });
+        }
+
+        loadMoreLocations() {
+            // calculate next page number
+            var newPage = Math.round(this.latestLocations.length / 7);
+            this.LocationService.getLatestLocations(6, newPage)
+            .then(result => {
+                    this.decorateLocationsWithUser(result.data);
+                    this.latestLocations = this.latestLocations.concat(result.data);
+                });
+        }
+
+        private decorateLocationsWithUser(locations:any) {
+            locations.forEach((item:any) => {
+                item.city.title = item.city.title.split(',')[0];
+                this.UserService.getUser(item.userid).then(result => {
+                    item.user = result.data;
+                });
+            });
         }
 
         static controllerId:string = "WelcomeFeedCtrl";
