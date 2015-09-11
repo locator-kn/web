0b010fa20655fc6730e1fd17a1b1ea17907065ff
Timo Weiß <info@timo-weiss.com>
2015-09-11 15:26:43 +0200

    add doNothing bool, extend $inject

--- www-develop/js/service/keenService.ts
+++ www-develop/js/service/keenService.ts
@@ -2,20 +2,24 @@ module Service {
     declare var Keen;
     export class KeenService {
 
-        static $inject = [];
 
         keenEvents:any = {
-            'pv': 'page_view',
-            'v': 'visit',
-            'sh': 'schoen_hier',
-            'lu': 'location_update',
-            'pu': 'profile_update',
-            'tu': 'trip_update'
-        };
+        'pv': 'page_view',
+        'v': 'visit',
+        'sh': 'schoen_hier',
+        'lu': 'location_update',
+        'pu': 'profile_update',
+        'tu': 'trip_update'
+    };
 
         client:any;
+        doNothing:boolean = false;
+
+        static $inject = ['KEEN_PROJECT_ID', 'KEEN_WRITE_KEY'];
 
         constructor(private KEEN_PROJECT_ID, private KEEN_WRITE_KEY) {
+
+            this.doNothing = window.location.host.indexOf('localhost') >= 0 || window.location.host.indexOf('amazonaws') >= 0
 
             this.client = new Keen({
                 projectId: this.KEEN_PROJECT_ID,
