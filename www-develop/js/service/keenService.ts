f0e813922669ac60e960c7183578c8c9f49f8a47
Timo Weiß <info@timo-weiss.com>
2015-09-11 15:01:12 +0200

    init keenEvents

--- www-develop/js/service/keenService.ts
+++ www-develop/js/service/keenService.ts
@@ -3,7 +3,17 @@ module Service {
 
         static $inject = [];
 
+        keenEvents:any = {
+            'pv': 'page_view',
+            'v': 'visit',
+            'sh': 'schoen_hier',
+            'lu': 'location_update',
+            'pu': 'profile_update',
+            'tu': 'trip_update'
+        };
+
         constructor() {
+
         }
 
         static serviceId:string = "KeenService";
