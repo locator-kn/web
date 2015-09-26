0def1754598df6acd10eaf4442b4922a89b9e991
Michael Knoch <info@michaelknoch.de>
2015-09-26 12:36:32 +0200

    explorectrl init

---
+++ www-develop/js/controller/exploreCtrl.ts
@@ -0,0 +1,12 @@
+module Controller {
+
+    export class ExploreCtrl {
+
+        static $inject = ['$state'];
+        constructor(private $state) {
+
+        }
+
+        static controllerId:string = "ExploreCtrl";
+    }
+}
