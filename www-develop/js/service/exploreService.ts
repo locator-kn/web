e89aa2c02ff8a251986eb5857f819bb85bb6abc5
Michael Knoch <info@michaelknoch.de>
2015-09-28 15:57:02 +0200

    implement searchLocations

--- www-develop/js/service/exploreService.ts
+++ www-develop/js/service/exploreService.ts
@@ -1,9 +1,13 @@
 module Service {
     export class ExploreService {
 
-        static $inject = ['$http'];
-        constructor(private $http) {
+        static $inject = ['$http', 'basePath'];
 
+        constructor(private $http, private basePath) {
+        }
+
+        searchLocations(cityId, category) {
+            return this.$http.get(this.basePath + '/locations/search/' + cityId + '/' + category);
         }
 
         static serviceId:string = "ExploreService";
