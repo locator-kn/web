af0d647b3e2f758071f92ce7b9567eff4a4b9e2f
Michael Knoch <info@michaelknoch.de>
2015-09-28 16:42:56 +0200

    implement location search

--- www-develop/js/controller/exploreCtrl.ts
+++ www-develop/js/controller/exploreCtrl.ts
@@ -3,13 +3,24 @@ module Controller {
     export class ExploreCtrl {
 
         locations = [];
+        cityId = '';
+        category = '';
+        query:any = {};
+        availableCities;
 
-        static $inject = ['$state', 'ExploreService'];
-        constructor(private $state, private ExploreService) {
+        static $inject = ['$state', 'ExploreService', '$scope', '$rootScope', '$location', 'DataService'];
+        constructor(private $state, private ExploreService, private $scope, private $rootScope, private $location, private DataService) {
 
+            this.query = $location.search();
+            this.cityId = this.$state.params.cityId;
+            this.category = this.$state.params.category;
 
-            this.ExploreService.searchLocations('ChIJWx8MOBv2mkcR0JnfpbdrHwQ', 'gastro').then(result => {
+            this.ExploreService.searchLocations(this.cityId, this.category).then(result => {
                 this.locations = result.data;
+            });
+
+            this.DataService.getLocationCities().then(result => {
+                this.availableCities = result.data;
             });
 
         }
